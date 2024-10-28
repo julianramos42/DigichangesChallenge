408cb5c09de44bba0040159694dc9c082afd3f70
Julián Ramos <julian26.ramos@gmail.com>
2024-10-28 18:06:35 -0300

    pagina inicio

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Head from "next/head";
 import Header from "./components/Header/Header";
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ subsets: ['latin'], weight: '400' });
 
 export const metadata: Metadata = {
   title: "Digichanges Challenge",
@@ -15,11 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-      </Head>
       <body className="min-h-screen">
         <Header/>
         {children}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="md:h-[90vh] flex flex-col justify-center items-center">
-      <h1>Bienvenido</h1>
+      <h1 className="text-3xl font-bold">Welcome to the Homepage!</h1>
+      <p>Explore the different sections to discover information about the Star Wars universe.</p>
     </main>
   );
 }
