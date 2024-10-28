ae7635aab970da78e639d61f38e644e48a19c005
Julián Ramos <julian26.ramos@gmail.com>
2024-10-28 05:29:10 -0300

    se actualiza filtro

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,12 @@ import '../config/database'
 import { errorHandler, errorNotFound } from './middlewares/errors';
 import indexRouter from './routes/index';
 import cronJob from './cron';
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/api', indexRouter);
 
--- backend/src/controllers/people/get.ts
+++ backend/src/controllers/people/get.ts
@@ -3,8 +3,6 @@ import getPeopleFromDB from "../../services/people/getFromDB";
 
 interface PeopleFilters {
     name?: RegExp;
-    height?: RegExp;
-    mass?: RegExp;
     gender?: RegExp;
     url?: string;
 }
@@ -19,14 +17,6 @@ export default async function getPeople(req: Request, res: Response, next: NextF
         };
 
         let skip = (pagination.page - 1) * pagination.limit;
-
-        if (typeof req.query.mass === "string") {
-            filter.mass = new RegExp(req.query.mass.trim(), "i");
-        }
-
-        if (typeof req.query.height === "string") {
-            filter.height = new RegExp(req.query.height.trim(), "i");
-        }
 
         if (typeof req.query.name === "string") {
             filter.name = new RegExp(req.query.name.trim(), "i");
