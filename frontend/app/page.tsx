ad0df899241755b9916aca0281b537c428df3864
Julián Ramos <julian26.ramos@gmail.com>
2024-10-28 18:09:23 -0300

    completo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="md:h-[90vh] flex flex-col justify-center items-center">
+    <main className="h-[90vh] flex flex-col justify-center items-center p-4 gap-4 text-center">
       <h1 className="text-3xl font-bold">Welcome to the Homepage!</h1>
       <p>Explore the different sections to discover information about the Star Wars universe.</p>
     </main>
